e4561bb26be79cd2a99e9581e4d21fae74250b6d
Artur Gubaidullin <lapteuh@gmail.com>
2025-01-08 18:02:24 +0000

    chore(examples/orders): add orders service

--- examples/orders/src/lib.rs
+++ examples/orders/src/lib.rs
@@ -1,1 +1,16 @@
+pub struct Orders;
 
+impl Orders {
+    #[must_use]
+    pub const fn new() -> Self {
+        Self {}
+    }
+
+    pub const fn create(&self, id: usize) {}
+}
+
+impl Default for Orders {
+    fn default() -> Self {
+        Self::new()
+    }
+}
