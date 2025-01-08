1589db8f9e68dd1dfcb7d419cb9fd674d351c874
Artur Gubaidullin <lapteuh@gmail.com>
2025-01-08 19:49:27 +0000

    chore(examples/messages/order-created): add struct

--- examples/messages/order-created/src/lib.rs
+++ examples/messages/order-created/src/lib.rs
@@ -1,1 +1,24 @@
+use std::{fmt::Display, sync::Arc};
 
+#[derive(Debug, Clone)]
+pub struct OrderCreated {
+    id: Arc<usize>,
+}
+
+impl OrderCreated {
+    #[must_use]
+    pub fn new(id: usize) -> Self {
+        Self { id: Arc::new(id) }
+    }
+
+    #[must_use]
+    pub fn id(&self) -> &usize {
+        &self.id
+    }
+}
+
+impl Display for OrderCreated {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "order #{} created", self.id)
+    }
+}
