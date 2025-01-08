df1d6154e22630fac0042be046502e53489c4dc1
Artur Gubaidullin <lapteuh@gmail.com>
2025-01-08 19:49:42 +0000

    chore(examples/messages/je-created): add struct

--- examples/messages/je-created/src/lib.rs
+++ examples/messages/je-created/src/lib.rs
@@ -1,1 +1,24 @@
+use std::{fmt::Display, sync::Arc};
 
+#[derive(Debug, Clone)]
+pub struct JECreated {
+    id: Arc<usize>,
+}
+
+impl JECreated {
+    #[must_use]
+    pub fn new(id: usize) -> Self {
+        Self { id: Arc::new(id) }
+    }
+
+    #[must_use]
+    pub fn id(&self) -> &usize {
+        &self.id
+    }
+}
+
+impl Display for JECreated {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "journal entry #{} created", self.id)
+    }
+}
