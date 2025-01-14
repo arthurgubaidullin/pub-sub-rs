e3a1e01567510f8bc9f0e5187130438302128eec
Artur Gubaidullin <lapteuh@gmail.com>
2025-01-14 10:00:27 +0000

    feat(examples/simple): add example

--- examples/simple/src/main.rs
+++ examples/simple/src/main.rs
@@ -1,8 +1,31 @@
+use bookkeeping_example::Bookkeeping;
+use futures::join;
+use in_memory_pub_sub::{subscribers, Subscribers, Topic};
+use notifications_example::Notifications;
 use orders_example::Orders;
+use std::time::Duration;
+use tokio::{main, time::sleep};
 
-#[tokio::main]
+#[main]
 async fn main() {
-    let orders = Orders;
+    let mut order_created_topic = Topic::new();
+    let mut je_created_topic = Topic::new();
 
-    orders.create(1);
+    let notifications = Notifications;
+    let bookkeeping = Bookkeeping::new(je_created_topic.clone());
+    let orders = Orders::new(order_created_topic.clone());
+
+    let order_created_subscription = subscribers![notifications, bookkeeping];
+
+    je_created_topic.link(&notifications);
+    order_created_topic.link(&order_created_subscription);
+
+    join!(
+        async {
+            sleep(Duration::from_millis(1)).await;
+            orders.create(1).await;
+        },
+        orders.create(2),
+        orders.create(3)
+    );
 }
